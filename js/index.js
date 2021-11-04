f6e8c35c7179e58f9a91db0abdb6fb9fbe15768a
Nassir <nassir.bour@gmail.com>
2021-11-04 21:22:24 +0100

    clipboard function modification

--- js/index.js
+++ js/index.js
@@ -2,12 +2,29 @@ const nav = document.querySelector('nav');
 const burger = document.querySelector('.burger');
 const copyBtn = document.getElementById('copy-btn');
 
-copyBtn.addEventListener('click', function() {
-    var copyText = document.getElementById("mailInput");
+// copyBtn.addEventListener('click', function() {
+//     var copyText = document.getElementById("mailInput");
   
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
-    navigator.clipboard.writeText(copyText.value);
+//     copyText.select();
+//     copyText.setSelectionRange(0, 99999);
+//     navigator.clipboard.writeText(copyText.value);
   
-    alert("Email copié: " + copyText.value);
-});
+//     alert("Email copié: " + copyText.value);
+// });
+
+function copyText(htmlElement){
+    if (!htmlElement) {
+        return;
+    }
+    let elementText = html.innerText;
+    let inputElement = document.createElement('input');
+    inputElement.setAttribute('value', elementText);
+    document.body.appendChild(inputElement);
+    inputElement.select();
+    document.execCommand('copy');
+    inputElement.parentNode.removeChild(inputElement);
+}
+
+document.querySelector('#copy-btn').onclick = function(){
+    copyText(document.querySelector('#text'))
+}
