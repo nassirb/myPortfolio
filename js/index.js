974be9a3d9a1d3ac189796a549a19941eaa33a58
Nassir <nassir.bour@gmail.com>
2021-11-04 18:38:00 +0100

    correcting css btn copy

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const nav = document.querySelector('nav');
 const burger = document.querySelector('.burger');
-const copyBtn = document.querySelector('#copy-btn');
+const copyBtn = document.getElementById('#copy-btn');
 
 copyBtn.addEventListener('click', event => {
     event.preventDefault
