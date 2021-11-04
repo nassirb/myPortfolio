f7cf8536a623a9904c3446ca26cb1cf8bb50e251
Nassir <nassir.bour@gmail.com>
2021-11-04 19:47:47 +0100

    test event listener

--- js/index.js
+++ js/index.js
@@ -1,15 +1,19 @@
 const nav = document.querySelector('nav');
 const burger = document.querySelector('.burger');
-const copyBtn = document.getElementById('#copy-btn');
+const copyBtn = document.getElementById('copy-btn');
 
-copyBtn.addEventListener('click', function() {
+// copyBtn.addEventListener('click', function() {
+//     console.log("clicked");
+//     var copyText = document.getElementById("mailInput");
+  
+//     copyText.select();
+//     copyText.setSelectionRange(0, 99999);
+
+//     navigator.clipboard.writeText(copyText.value);
+  
+//     alert("Email copié: " + copyText.value);
+// });
+
+copyBtn.addEventListener('click', function(){
     console.log("clicked");
-    var copyText = document.getElementById("mailInput");
-  
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
-
-    navigator.clipboard.writeText(copyText.value);
-  
-    alert("Email copié: " + copyText.value);
 });
