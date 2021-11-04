2b501d13aaae581cf614a2904b2d2f788a0a391a
Nassir <nassir.bour@gmail.com>
2021-11-04 17:04:35 +0100

    adding images and implementing contact section

--- js/index.js
+++ js/index.js
@@ -1,2 +1,13 @@
 const nav = document.querySelector('nav');
 const burger = document.querySelector('.burger');
+
+function copyMail() {
+    var copyText = document.getElementById("myMail");
+  
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+
+    navigator.clipboard.writeText(copyText.value);
+  
+    alert("Email copié: " + copyText.value);
+}
